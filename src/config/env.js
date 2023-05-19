c3f8fef7ed22927edcccf19bb366bbcdf63bfa4f
Cauê Mendes <mendes.caue@outlook.com>
2023-05-18 22:40:25 -0300

    Env update

--- src/config/env.js
+++ src/config/env.js
@@ -1,19 +1,19 @@
 export default {
   // Local Teste
-  web: {
-    BASE: import.meta.env.VITE_BASE_URL || "http://localhost:1337",
-  },
-  api: {
-    URL: import.meta.env.VITE_API_URL || "http://localhost:1337/api",
-    BASE: import.meta.env.VITE_API_BASE || "http://localhost:1337",
-  },
-
-  // Original
   // web: {
-  //   BASE: import.meta.env.VITE_BASE_URL || "https://api.igcp.com.br",
+  //   BASE: import.meta.env.VITE_BASE_URL || "http://localhost:1337",
   // },
   // api: {
-  //   URL: import.meta.env.VITE_API_URL || "https://api.igcp.com.br/api",
-  //   BASE: import.meta.env.VITE_API_BASE || "https://api.igcp.com.br",
+  //   URL: import.meta.env.VITE_API_URL || "http://localhost:1337/api",
+  //   BASE: import.meta.env.VITE_API_BASE || "http://localhost:1337",
   // },
+
+  // Original
+  web: {
+    BASE: import.meta.env.VITE_BASE_URL || "https://api.igcp.com.br",
+  },
+  api: {
+    URL: import.meta.env.VITE_API_URL || "https://api.igcp.com.br/api",
+    BASE: import.meta.env.VITE_API_BASE || "https://api.igcp.com.br",
+  },
 };
